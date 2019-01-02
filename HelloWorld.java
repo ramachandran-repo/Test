a1c07ed7831d66d88e9f7f7aa742a1f78f364f0a
ramachandrans410 <45954767+ramachandrans410@users.noreply.github.com>
2019-01-02 05:40:00 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld(){
-	public static void main (String [] args){
-		System.out.println("Jenkins setup is working fine");
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
 	}
 }
